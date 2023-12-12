cc1af47df28a6e140a73a69c0e11b366ff76b75b
ben <b1bulbeck@student.ccn.ac.uk>
2023-12-12 10:06:26 +0000

    all the code

---
+++ Driving home for christmas/main.py
@@ -0,0 +1,54 @@
+import pygame, sys
+
+from settings import *
+from sprites import *
+
+class Game:
+    def __init__(self):
+        self.running = True
+
+        self.screen = pygame.display.set_mode((WIN_WIDTH, WIN_WIDTH))
+        pygame.display.set_caption("blue square that moves across the screen")
+        icon = pygame.image.load('assets/images/0000FF.png.png').convert_alpha()
+        pygame.display.set_icon(icon)
+        self.clock=pygame.time.Clock()
+
+
+        pygame.mixer.init()
+        pygame.mixer.music.load("assets/music/Driving Home For Christmas.mp3")
+        pygame.mixer.music.play(-1)
+
+    def draw(self):
+        self.screen.fill("gray")
+
+        self.all_sprites.draw(self.screen)
+        self.clock.tick(FPS)
+        pygame.display.update()
+
+    def events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+
+    def update(self):
+        self.all_sprites.update()
+
+    def main(self):
+        while self.running:
+            self.events()
+            self.draw()
+            self.update()
+            
+    def new(self):
+        self.all_sprites = pygame.sprite.LayeredUpdates()
+
+        self.player = Player(self, 1, 1)
+
+if __name__ == "__main__":
+    game = Game()
+
+    game.new()
+    game.main()
+
+    pygame.quit()
+    sys.exit()
