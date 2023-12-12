05c232b255be418b7f46083ba2b37e41d82c7079
ben <b1bulbeck@student.ccn.ac.uk>
2023-12-12 13:37:58 +0000

    124

--- Driving home for christmas/main.py
+++ Driving home for christmas/main.py
@@ -30,10 +30,16 @@ class Game:
                 if column == 'P':
                     Player(self,j,i)
 
+    def main_menu(self):
+        pass
+
     def events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.running = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    self.running = False
 
     def update(self):
         self.all_sprites.update()
